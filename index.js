52473a4826f9aa930cd2953483cb3ee85c826166
Beerscheba C <designergo@aol.com>
2018-06-20 20:05:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ return newObject
     function destructivelyDeleteFromObjectByKey(object, key){
       
       delete key.value
-      return true
+      
     }
   
   function deleteFromObjectByKey(object, key){
