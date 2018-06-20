4ef4a4d15b60aa76126168a8bcc36fe3961c9ab0
Beerscheba C <designergo@aol.com>
2018-06-20 19:14:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,46 +1,4 @@
-var recipes = {
-  eggs: 'three',
-  onions: 'one',
-  peppers: 'greens',
-  salt: 'pinch',
-  butter: 'tablespoon'
-  }
 
-function updateObjectWithKeyAndValue(object, key, value) {
-object[key] = value
- 
- return object
-  }
-  
-  function updateObjectWithKeyAndValue(object, key, value) {
- 
- object['key2'] = 'value2'
- 
-     
-        return object
-
-      }
-    
-   function  deleteFromObjectByKey(object, key){
-     delete key.eggs
-     return object
-   }
-    
-   function destructivelyUpdateObjectWithKeyAndValue(object, key, value){ 
-     
-     object[key]= value
-   return object}
-    
-    function destructivelyDeleteFromObjectByKey(object, key){
-      
-      delete key.salt
-      return true
-    }
-  
-  function function updateObjectWithKeyAndValue(object, key, value) {
-  var newObject =  Object.assign(object, {key:value} )
-return newObject 
-    }
   
   
   
