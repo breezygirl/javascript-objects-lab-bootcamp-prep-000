d35b5532149b11a2d42d02e599c02baaa043755e
Beerscheba C <designergo@aol.com>
2018-06-20 19:31:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,4 +21,18 @@ return newObject
      delete key.eggs
      return object
    }
+    function destructivelyUpdateObjectWithKeyAndValue(object, key, value){ 
+     
+     object[key]= value
+   return object}
     
+    function destructivelyDeleteFromObjectByKey(object, key){
+      
+      delete key.salt
+      return true
+    }
+  
+  function function updateObjectWithKeyAndValue(object, key, value) {
+  var newObject =  Object.assign({},object, {[key]:value} )
+return newObject 
+    }
